264a00317dbc22e8df321c4f0ddb4515070fa855
LiranBratt2121 <brattlirannin@gmail.com>
2023-12-28 13:32:07 +0200

    detector: Add checks if the creambo is in the desired threshold

--- detector.py
+++ detector.py
@@ -27,20 +27,19 @@ class Detector:
             text_scale=0.35
         )
 
-    def predict_and_annotate(self, name: str, confidence_threshold=0.6):
+    def predict_and_annotate(self, img: np.ndarray, confidence_threshold=0.6):
         """
         Performs object detection and annotation on the specified image.
 
         Args:
-        - name (str): Path to the image file.
+        - img (np.ndarray): Image frame captured from the camera.
         - confidence_threshold (float): Confidence threshold for object detection.
 
         Returns:
         - Annotated image (np.ndarray): Annotated image with bounding boxes and labels.
+        - dict<str, any>: dict that contains the xyxy, conf, class and the lable of the detected object 
         """
-        results = self.model.predict(name, conf=confidence_threshold)
-
-        img = cv2.imread(name)
+        results = self.model.predict(img, conf=confidence_threshold, verbose=False)
 
         detections = sv.Detections.from_yolov8(results[0])
 
@@ -56,22 +55,89 @@ class Detector:
             labels=labels,
         )
 
-        return annotated_img
+        return annotated_img, {"XYXY": detections.xyxy, "CONFIDENCE": detections.confidence, "CLASS": detections.class_id, "LABEL": labels}
 
-    def show(self, name: str, img: np.ndarray):
+    def draw_threshold(self, frame, line_delta: int = 200):
         """
-        Displays the annotated image.
+        Draws 2 lines which show the safe distance in which the creambo needs to be at.
 
         Args:
-        - name (str): Name of the window to display.
-        - img (np.ndarray): Annotated image to display.
+        - frame (np.ndarray): Input frame to draw lines on.
+        - line_delta (int): The delta distance between the lines. Default is 200.
 
         Returns:
-        - Annotated image (np.ndarray): The input annotated image.
+        - frame (np.ndarray): Frame with drawn lines.
         """
-        cv2.imshow(name, img)
+        height, width, _ = frame.shape
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        middle_height = height // 2
+        added_distance = line_delta // 2
 
-        return img
+        cv2.line(frame, (0, middle_height - added_distance),
+                 (width, middle_height - added_distance), (0, 255, 0), 2)
+        cv2.line(frame, (0, middle_height + added_distance),
+                 (width, middle_height + added_distance), (255, 0, 0), 2)
+
+        return frame
+
+    def __threashold_check(self, frame, center_y: int, line_delta: int = 200):
+        """
+        Checks if the creambo is in the safe distance.
+
+        Args:
+        - frame (np.ndarray): Input frame to check.
+        - center_y (int): The y coordinate of the creambo center.
+        - line_delta (int): The delta distance between the lines. Default is 200.
+
+        Returns:
+        - bool: True if the creambo is in the safe distance, False otherwise.
+        - float: The difference between the safe distance middle point and the creambo's current position.
+        """
+        height, _, _ = frame.shape
+
+        middle_height = height // 2
+        added_distance = line_delta // 2
+
+        lower_line_height = middle_height + added_distance
+        upper_line_height = middle_height - added_distance
+
+        distance_from_middle = center_y - middle_height
+
+        if lower_line_height < center_y < upper_line_height:
+            return True, distance_from_middle
+
+        return False, distance_from_middle
+
+    def apply_threashold(self, frame, center_y, line_delta=200, move_distance=50, font=cv2.FONT_HERSHEY_SIMPLEX,
+                         font_scale=0.7, font_thickness=2):
+        """
+        Determines and displays the action needed based on the detected object's position.
+
+        Args:
+        - frame (np.ndarray): Input frame to check.
+        - center_y (int): The y coordinate of the creambo center.
+        - line_delta (int): The delta distance between the lines. Default is 200.
+        - move_distance (int): Desired distance to move up/down. Default is 50.
+        - font (int): Font type for cv2.putText. Default is cv2.FONT_HERSHEY_SIMPLEX.
+        - font_scale (float): Font scale for cv2.putText. Default is 0.7.
+        - font_thickness (int): Font thickness for cv2.putText. Default is 2.
+
+        Returns:
+        - str: Action message based on the detected object's position.
+        """
+        is_safe, distance = self.__threashold_check(
+            frame, center_y, line_delta)
+
+        action = ""
+        if is_safe:
+            action = "Creambo is within the safe distance!"
+        else:
+            if distance > 0:
+                action = f"Move Creambo up by {abs(distance)} pixels"
+            else:
+                action = f"Move Creambo down by {abs(distance)} pixels"
+
+        cv2.putText(frame, action, (20, 50), font, font_scale,
+                    (255, 255, 255), font_thickness)
+
+        return action
